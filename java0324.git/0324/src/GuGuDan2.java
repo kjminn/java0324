cffdbf8b34d4be66724908dd100cb11aa542ccdc
YUHAN <YUHAN@172.17.50.132>
2021-03-24 15:01:35 +0900

    세로방향 구구단

---
+++ java0324.git/0324/src/GuGuDan2.java
@@ -0,0 +1,14 @@
+
+public class GuGuDan2 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			for (int j = 2; j < 10; j++) {
+				System.out.printf("%d*%d=%d\t", j, i, j*i);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
